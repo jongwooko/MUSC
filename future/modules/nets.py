226bfbb60a75b202e0ab8f30f24bf8878688f92f
root <jongwoo.ko@kaist.ac.kr>
2022-02-25 14:11:47 +0000

    [Add] Functions for embedding/hidden outputs of Bert

--- future/modules/nets.py
+++ future/modules/nets.py
@@ -1,6 +1,6 @@
-from transformers import BertModel, BertPreTrainedModel
-from transformers.modeling_bert import (
-    BertForSequenceClassification,
+from .bert_modeling import (
+    BertModel, 
+    BertPreTrainedModel,
     BertForMultipleChoice,
 )
 from transformers.configuration_roberta import RobertaConfig
@@ -10,6 +10,118 @@ from transformers.tokenization_roberta import RobertaTokenizer
 import torch.nn as nn
 import torch
 
+class BertForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertForSequenceClassification, self).__init__(config)
+        self.num_labels = config.num_labels
+        
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+        
+        self.init_weights()
+        
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+    ):
+        
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), logits, (hidden_states), (attentions)
+    
+    def get_embedding_output(
+        self,
+        input_ids,
+        token_type_ids=None,
+        position_ids=None,
+    ):
+        return self.bert.get_embedding_output(input_ids=input_ids, token_type_ids=token_type_ids,
+                                              position_ids=position_ids)
+    
+    def get_logits_from_embedding_output(
+        self,
+        embedding_output,
+        attention_mask=None,
+        labels=None
+    ):
+        outputs = self.bert.get_bert_output(embedding_output, attention_mask=attention_mask)
+        sequence_output = outputs[0]
+        logits = self.classifier(sequence_output)
+        outputs = (logits,) + outputs[2:]
+        if labels is not None:
+            if labels is not None:
+                if self.num_labels == 1:
+                    #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+        return outputs  # (loss), logits, (hidden_states), (attentions)
+    
+    def get_last_hidden_from_embedding_output(
+        self,
+        embedding_output,
+        attention_mask=None,
+        labels=None
+    ):
+        outputs = self.bert.get_bert_output(embedding_output, attention_mask=attention_mask)
+        sequence_output = outputs[0]
+        logits = self.classifier(sequence_output)
+        return sequence_output, logits
+    
+    def get_last_hidden(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+    ):
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds
+        )
+        sequence_output = outputs[0]
+        logits = self.classifier(sequence_output)
+        return sequence_output, logits
 
 class LinearPredictor(BertPreTrainedModel):
     def __init__(self, bert_config, out_dim, dropout):
@@ -21,8 +133,7 @@ class LinearPredictor(BertPreTrainedModel):
 
     def forward(self):
         raise NotImplementedError
-
-
+        
 class BertForSequenceTagging(LinearPredictor):
     """
     used for both tagging and ner.
@@ -50,6 +161,83 @@ class BertForSequenceTagging(LinearPredictor):
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
         )
+        bert_out = bert_out[0]
+        bert_out = self.dropout(bert_out)
+        bert_out = self.classifier(bert_out)
+        logits = bert_out[if_tgts]
+        return (
+            logits,
+            torch.argmax(bert_out, dim=-1, keepdim=False),
+            bert_out,
+        )
+    
+    def get_embedding_output(
+        self,
+        input_ids=None,
+        token_type_ids=None,
+        position_ids=None,
+    ):
+        return self.bert.get_embedding_output(input_ids=input_ids, token_type_ids=token_type_ids,
+                                              position_ids=position_ids)
+    
+    def get_logits_from_embedding_output(
+        self,
+        embedding_output,
+        attention_mask=None,
+        labels=None,
+        if_tgts=None,
+    ):
+        bert_out = self.bert.get_bert_output(
+                embedding_output,
+                attention_mask=attention_mask
+        )
+        
+        bert_out = bert_out[0]
+        bert_out = self.dropout(bert_out)
+        bert_out = bert_out[if_tgts]
+        return (
+            logits,
+            torch.argmax(bert_out, dim=-1, keepdim=False),
+            bert_out,
+        )
+    
+    def get_last_hidden_from_embedding_output(
+        self,
+        embedding_output,
+        attention_mask=None,
+        labels=None,
+    ):
+        bert_out = self.bert.get_bert_output(
+            embedding_output,
+            attention_mask=attention_mask
+        )
+        bert_out = bert_out[0]
+        bert_out = self.dropout(bert_out)
+        bert_out = bert_out[if_tgts]
+        return (
+            logits,
+            torch.argmax(bert_out, dim=-1, keepdim=False),
+            bert_out,
+        )
+    
+    def get_last_hidden(
+        self,
+        input_ids=None,
+        attention_mas=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+    ):
+        bert_out = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
+        
         bert_out = bert_out[0]
         bert_out = self.dropout(bert_out)
         bert_out = self.classifier(bert_out)
