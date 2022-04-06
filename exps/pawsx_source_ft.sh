303088c84a83cd773389ccafb8ed194622772f8d
Jaehoon Oh <jhoon.oh@kaist.ac.kr>
2022-04-06 12:07:55 +0000

    [Add] Generate mislabeled & imbalanced dataset

--- exps/pawsx_source_ft.sh
+++ exps/pawsx_source_ft.sh
@@ -14,14 +14,62 @@
 #         --finetune_lr 1e-5 \
 #         --model bert-base-multilingual-cased \
 
-python finetuning_baseline.py --dataset_name pawsx \
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py \
+                              --dataset_name pawsx \
+                              --experiment mislabel_ratio_0.0 \
                               --trn_languages english \
-                              --eval_languages english,german,chinese,french,japanese,korean,spanish \
+                              --eval_languages english \
                               --finetune_epochs 10 \
                               --finetune_batch_size 32 \
-                              --eval_every_batch 50 \
+                              --eval_every_batch 500 \
                               --override False \
-                              --train_fast True \
+                              --train_fast False \
                               --max_seq_len 128 \
-                              --world 0 \
-                              --finetune_lr 1e-5
+                              --finetune_lr 1e-5 \
+                              --mislabel_type uniform \
+                              --mislabel_ratio 0.0
+                              
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py \
+                              --dataset_name pawsx \
+                              --experiment mislabel_ratio_0.2 \
+                              --trn_languages english \
+                              --eval_languages english \
+                              --finetune_epochs 10 \
+                              --finetune_batch_size 32 \
+                              --eval_every_batch 500 \
+                              --override False \
+                              --train_fast False \
+                              --max_seq_len 128 \
+                              --finetune_lr 1e-5 \
+                              --mislabel_type uniform \
+                              --mislabel_ratio 0.2
+
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py \
+                              --dataset_name pawsx \
+                              --experiment mislabel_ratio_0.3 \
+                              --trn_languages english \
+                              --eval_languages english \
+                              --finetune_epochs 10 \
+                              --finetune_batch_size 32 \
+                              --eval_every_batch 500 \
+                              --override False \
+                              --train_fast False \
+                              --max_seq_len 128 \
+                              --finetune_lr 1e-5 \
+                              --mislabel_type uniform \
+                              --mislabel_ratio 0.3
+                              
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py \
+                              --dataset_name pawsx \
+                              --experiment mislabel_ratio_0.4 \
+                              --trn_languages english \
+                              --eval_languages english \
+                              --finetune_epochs 10 \
+                              --finetune_batch_size 32 \
+                              --eval_every_batch 500 \
+                              --override False \
+                              --train_fast False \
+                              --max_seq_len 128 \
+                              --finetune_lr 1e-5 \
+                              --mislabel_type uniform \
+                              --mislabel_ratio 0.4
