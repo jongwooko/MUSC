6ce2fb0cfd60dd6bfabeca0f9e5a8413e0ad929f
Jaehoon Oh <jhoon.oh@kaist.ac.kr>
2022-04-06 11:40:59 +0000

    [Fix] shell files for evaluation per epoch

--- exps/pawsx_source_ft.sh
+++ exps/pawsx_source_ft.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-# CUDA_VISIBLE_DEVICES=0
+
+# 1 epoch = 1544 batches (batch size: 32)
+
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py --dataset_name pawsx \
+                                                     --experiment source_epoch \
+                                                     --trn_languages english \
+                                                     --eval_languages english,german,chinese,french,japanese,korean,spanish \
+                                                     --finetune_epochs 10 \
+                                                     --finetune_batch_size 32 \
+                                                     --eval_every_batch 1544 \
+                                                     --override False \
+                                                     --train_fast True \
+                                                     --max_seq_len 128 \
+                                                     --world 0 \
+                                                     --finetune_lr 1e-5 \
+                                                     --model bert-base-multilingual-cased
 
 # python -m torch.distributed.launch --nproc_per_node=4 \
 #         finetuning_baseline.py --dataset_name pawsx \
@@ -13,15 +28,3 @@
 #         --world 0 \
 #         --finetune_lr 1e-5 \
 #         --model bert-base-multilingual-cased \
-
-python finetuning_baseline.py --dataset_name pawsx \
-                              --trn_languages english \
-                              --eval_languages english,german,chinese,french,japanese,korean,spanish \
-                              --finetune_epochs 10 \
-                              --finetune_batch_size 32 \
-                              --eval_every_batch 50 \
-                              --override False \
-                              --train_fast True \
-                              --max_seq_len 128 \
-                              --world 0 \
-                              --finetune_lr 1e-5
