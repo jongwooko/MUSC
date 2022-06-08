75117e8640cc39f702d8f7de99cf6d44302dc90b
root <jongwoo.ko@kaist.ac.kr>
2022-06-08 04:56:46 +0000

    [Fix] Fix SupCon Loss for Single-View

--- future/modules/losses.py
+++ future/modules/losses.py
@@ -88,7 +88,7 @@ class SupConLoss(nn.Module):
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
 
         # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+        mean_log_prob_pos = (mask * log_prob).sum(1) / (mask.sum(1) + 1e-4)
 
         # loss
         loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
