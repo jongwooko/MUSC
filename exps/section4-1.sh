1c5d30990ac144f8d3a4a35fde63fd2269762bfa
jaehoon <jhoon.oh@kaist.ac.kr>
2022-06-25 07:38:25 +0000

    [fix] shell files

---
+++ exps/section4-1.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# 1 epoch = 12272 batches (batch size: 32)
+                    
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py --dataset_name marc \
+                     --experiment marc_musc \
+                     --train_mt \
+                     --trn_languages german,chinese,french,japanese,spanish \
+                     --eval_languages english,german,chinese,french,japanese,spanish \
+                     --finetune_epochs 4 \
+                     --finetune_batch_size 32 \
+                     --eval_every_batch 300 \
+                     --override False \
+                     --train_fast False \
+                     --world 0 \
+                     --finetune_lr 1e-5 \
+                     --model bert-base-multilingual-cased \
+                     --use_mix \
+                     --use_supcon \
+                     --lam 0.1
+                     
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py --dataset_name mldoc \
+                     --experiment mldoc10000_musc \
+                     --train_mt \
+                     --trn_languages japanese,chinese,french,german,spanish,russian,italian \
+                     --eval_languages english,japanese,chinese,french,german,spanish,russian,italian \
+                     --finetune_epochs 10 \
+                     --finetune_batch_size 32 \
+                     --eval_every_batch 300 \
+                     --override False \
+                     --train_fast False \
+                     --world 0 \
+                     --finetune_lr 1e-5 \
+                     --model bert-base-multilingual-cased \
+                     --use_mix \
+                     --use_supcon \
+                     --lam 0.1
+
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py --dataset_name pawsx \
+                     --experiment pawsx_musc \
+                     --trans_train \
+                     --trn_languages chinese,french,german,japanese,korean,spanish \
+                     --eval_languages english,chinese,french,german,japanese,korean,spanish \
+                     --finetune_epochs 4 \
+                     --finetune_batch_size 32 \
+                     --eval_every_batch 500 \
+                     --override False \
+                     --train_fast False \
+                     --max_seq_len 128 \
+                     --world 0 \
+                     --finetune_lr 3e-5 \
+                     --model bert-base-multilingual-cased \
+                     --use_mix \
+                     --use_supcon \
+                     --lam 0.1
+                     
+CUDA_VISIBLE_DEVICES=0 python finetuning_baseline.py --dataset_name xnli \
+                     --experiment xnli_musc \
+                     --trans_train \
+                     --trn_languages arabic,bulgarian,chinese,french,german,greek,hindi,russian,spanish,swahili,thai,turkish,urdu,vietnamese \
+                     --eval_languages english,arabic,bulgarian,chinese,french,german,greek,hindi,russian,spanish,swahili,thai,turkish,urdu,vietnamese \
+                     --finetune_epochs 2 \
+                     --finetune_batch_size 32 \
+                     --eval_every_batch 3000 \
+                     --override False \
+                     --train_fast False \
+                     --max_seq_len 128 \
+                     --world 0 \
+                     --finetune_lr 3e-5 \
+                     --model bert-base-multilingual-cased \
+                     --use_mix \
+                     --use_supcon \
+                     --lam 0.1
